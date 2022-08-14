ab2202ca49e04c88076470d7787394cf5e70e105
talha-khattak <109832903+talha-khattak@users.noreply.github.com>
2022-08-14 00:04:43 -0400

    Create brutecaesar.cpp

---
+++ brutecaesar.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	
+    string input;
+    getline(cin, input);
+    
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    
+    for(int i = 0; i < alphabet.length(); i++) {
+       
+        cout << i << ": ";
+        
+        for(int j = 0; j < input.length(); j++) {
+            
+            if(input[j] == ' ') {
+              
+                cout << " ";
+                
+            }
+            else {
+            	
+                cout << alphabet[(alphabet.find(input[j]) + i) % alphabet.length()];
+                
+            }
+            
+        }
+        
+        cout << endl;
+        
+    }
+	
+}
