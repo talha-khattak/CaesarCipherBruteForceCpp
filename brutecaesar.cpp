0c24e18bddfe367dd9789e8751f0c92677df0e1f
talha-khattak <109832903+talha-khattak@users.noreply.github.com>
2022-08-16 16:56:15 -0400

    Update brutecaesar.cpp
    Got rid of unnecessary blank lines

--- brutecaesar.cpp
+++ brutecaesar.cpp
@@ -4,33 +4,20 @@
 using namespace std;
 
 int main() {
-	
     string input;
     getline(cin, input);
-    
     string alphabet = "abcdefghijklmnopqrstuvwxyz";
-    
+
     for(int i = 0; i < alphabet.length(); i++) {
-       
         cout << i << ": ";
-        
         for(int j = 0; j < input.length(); j++) {
-            
             if(input[j] == ' ') {
-              
                 cout << " ";
-                
             }
             else {
-            	
                 cout << alphabet[(alphabet.find(input[j]) + i) % alphabet.length()];
-                
             }
-            
         }
-        
         cout << endl;
-        
     }
-	
 }
